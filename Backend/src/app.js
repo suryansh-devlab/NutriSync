d06493592950c4398a8fcf3f9dc6cbdaa3717851
Suryansh Kushwaha <suryanshkushwaha1235@gmail.com>
2026-06-06 17:16:38 +0530

    feat: add routes for wishlist, reviews, and addresses in the main application

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,6 +4,9 @@ import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import cartRoutes from "./routes/cart.routes.js";
 import orderRoutes from "./routes/order.routes.js";
+import wishlistRoutes from "./routes/wishlist.routes.js";
+import reviewRoutes from "./routes/review.routes.js";
+import addressRoutes from "./routes/address.routes.js";
 const app = express();
 
 app.use(express.json());
@@ -11,6 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/addresses", addressRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/wishlist", wishlistRoutes);
+app.use("/api/reviews", reviewRoutes);
 export default app;
